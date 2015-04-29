fa3e4ebbfb49c249d3d264ac79dc0f312fbb4a6d
Juzet Arana <arana.juzet@gmail.com>
2015-04-29 10:48:55 -0700

    done with quiz

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.dan').click(function(){
+
+	alert("You clicked me!");
+
+	});
+
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function(){
+	$('#iwao').hide();
+
+	});
+
+});
